750c17a39f6a3f46f4c1faae1c9fdba76de7ae08
Max Tang <tw7613781@gmail.com>
2020-05-12 01:04:22 +0900

    feat: twoSum O(n)

--- Golang/twoSum.go
+++ Golang/twoSum.go
@@ -1,21 +1,13 @@
 package leetcode
 
-import "sort"
-
-func search(nums []int, target int) int {
-	index := sort.SearchInts(nums, target)
-	if nums[index] == target {
-		return index
-	}
-	return -1
-}
-
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
-		k := search(nums[i+1:], target-nums[i])
-		if k != -1 {
-			return []int{i, k}
+	cache := make(map[int]int)
+	for i, v := range nums {
+		w := target - v
+		if j, ok := cache[w]; ok {
+			return []int{j, i}
 		}
+		cache[v] = i
 	}
-	return []int{0, 0}
+	return []int{}
 }
--- Golang/twoSum_test.go
+++ Golang/twoSum_test.go
@@ -1,1 +1,20 @@
 package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwoSum(t *testing.T) {
+	/*****case 1*****/
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	res := twoSum(nums, target)
+	assert.Equal(t, []int{0, 1}, res, "")
+	/*****case 2*****/
+	nums1 := []int{0, 1, 5, 10}
+	target1 := 11
+	res1 := twoSum(nums1, target1)
+	assert.Equal(t, []int{1, 3}, res1, "")
+}
